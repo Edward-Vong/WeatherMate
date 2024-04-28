83a54b983d71636052bb30e5b6371d135efc4b81
Edward Vong <evusa142@gmail.com>
2024-04-28 00:54:05 -0700

    removed unnecessay code

--- weathermate/src/main/java/app/weatherVisualController.java
+++ weathermate/src/main/java/app/weatherVisualController.java
@@ -7,7 +7,6 @@ import java.util.HashMap;
 import org.json.JSONObject;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
